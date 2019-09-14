7baf10060ed09e01561dfbf6870cff0424005182
warrenm16 <54646159+warrenm16@users.noreply.github.com>
2019-09-14 10:15:12 -0700

    done

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -6,9 +6,9 @@ int main()
 {
   // TODO#1: try changing these values to others found in the ASCII character set
   // The class Quick Reference Guide contains this chart on page 5
-  char my_char1 = 67;   // decimal
-  char my_char2 = 0x43; // hex
-  char my_char3 = 'C';  // character
+  char my_char1 = 75;   // decimal
+  char my_char2 = 0x4B; // hex
+  char my_char3 = 'K';  // character
 
   std::cout << "Char1: " << my_char1 << std::endl;
   std::cout << "Char2: " << my_char2 << std::endl;
@@ -22,24 +22,36 @@ int main()
   // hint: use the ASCII character set
   std::cout << "+" << std::endl;
 
+  char number;
+  number = 43;
+  std::cout << number << std::endl;
+
+  char symbol;
+  symbol = '+';
+  std::cout << symbol << std::endl;
+
+  char hexi;
+  hexi = 0x2B;
+  std::cout << hexi << std::endl;
+
   std::cout << std::endl; // blank line to separate the different exercises
 
   // TODO#3: update the following statement to include escape characters
   // note that clang does not require an escape character on the single quote
   // but many other compilers do require it
-  std::cout << "'Y'" << std::endl;
+  std::cout << 'Y' << std::endl;
 
   std::cout << std::endl; // blank line to separate the different exercises
 
   // TODO#4: try changing the variable types and the data types in the
   // statements below to see what is returned by the sizeof operator
-  bool my_bool = 1;
-  std::cout << my_bool << ' ' << sizeof(my_bool) << '\n';
-  short my_int = 1;
+  int my_int = 2;
   std::cout << my_int << ' ' << sizeof(my_int) << '\n';
+  long my_long = 2;
+  std::cout << my_int << ' ' << sizeof(my_long) << '\n';
 
   double amount;
-  std::cout << "A float is stored in " << sizeof(float) << " bytes\n";
+  std::cout << "A long long is stored in " << sizeof(long long) << " bytes\n";
   std::cout << "The variable \"amount\" is stored in " << sizeof(amount) << " bytes\n";
 
   return 0;
